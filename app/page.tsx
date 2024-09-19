f48a36fba25141361e913d8b72371e2b634f9d78
ss251 <ss251@uw.edu>
2024-09-20 04:02:03 +0800

    feat: add link to Farcaster frame on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { getFrameMetadata } from 'frog/next'
 import type { Metadata } from 'next'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export async function generateMetadata(): Promise<Metadata> {
   const url = process.env.VERCEL_URL || 'http://localhost:3000'
@@ -16,8 +17,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Welcome to Stat Frame</h1>
-      <p>This is a web application for exploring Farcaster statistics.</p>
-      <p>To interact with the Frame, visit this page on a Farcaster client.</p>
+      <p>This is a Frame server for exploring Farcaster user statistics within frames.</p>
+      <p>To interact with the Frame, check it out on <Link  target="_blank" className='text-purple-500 underline' href="https://warpcast.com/thescoho/0xf1d1006d">Warpcast</Link></p>
     </main>
   )
 }
